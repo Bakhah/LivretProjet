924eebb5fa05d80b6b0015e7ddc13e3e886feda1
bakhah <franck.hourdin@pictotravel.fr>
2016-09-28 19:45:57 +0200

    Fix some authentication traductions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users
   root to: "home#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
