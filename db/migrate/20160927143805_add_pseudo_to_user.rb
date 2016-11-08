eaff2d02f0d8af2b67f7d942c346c0f79575795f
bakhah <franck.hourdin@pictotravel.fr>
2016-11-08 11:26:43 +0100

    fix

--- db/migrate/20160927143805_add_pseudo_to_user.rb
+++
@@ -1,9 +0,0 @@
-class AddPseudoToUser < ActiveRecord::Migration
-  def self.up
-    add_column :monthly_self_examinations, :id, :primary_key
-  end
-
-  def self.down
-    remove_column :monthly_self_examinations, :id
-  end
-end
