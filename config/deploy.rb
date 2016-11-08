110ee18c22c6f35d4b49a094d480663cdf1a4cbb
bakhah <franck.hourdin@pictotravel.fr>
2016-11-08 15:51:28 +0100

    add linked_file

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,7 @@ set :branch, "master"
 # set :keep_releases, 5
 set :rails_env, "production"
 
-set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml db/production.sqlite3}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 set :rvm_ruby_version, '2.3.1'
